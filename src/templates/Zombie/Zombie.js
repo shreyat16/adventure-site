e032d6b8d175e34118cca7fc48dedd207e92514b
Shreya <sthamm2001@gmail.com>
2020-06-25 20:15:25 -0400

    v6

--- src/templates/Zombie/Zombie.js
+++ src/templates/Zombie/Zombie.js
@@ -46,27 +46,36 @@ export const query = graphql`
 
 export default function Zombie({data}) {
   return (
-    <div className={`zombies-${data.current.fields.name}`}>
-      {data.current.frontmatter.featuredImage && (
-        <Img className="zombie__featured-image" fluid={data.current.frontmatter.featuredImage.childImageSharp.fluid} />
-      )}
-      <h1>{data.current.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{__html: data.current.html}} />
-      {data.choiceOne && (
-        <div>
-        <Link style={{color: "#000000"}} to={data.choiceOne.fields.slug}>{data.choiceOne.frontmatter.title}</Link>
+    <div className="Zombies">
+        <div className={`zombies-${data.current.fields.name}`}>
+        {data.current.frontmatter.featuredImage && (
+          <Img className="zombie__featured-image" fluid={data.current.frontmatter.featuredImage.childImageSharp.fluid} />
+        )}
+        <div className="titlestyle">
+          <h1>You chose: {data.current.frontmatter.title}</h1>
         </div>
-      )}
-      {data.choiceTwo && (
-        <div>
-          <Link style={{color: "#000000"}} to={data.choiceTwo.fields.slug}>{data.choiceTwo.frontmatter.title}</Link>
+        <div dangerouslySetInnerHTML={{__html: data.current.html}} />
+        <div className="choicesstyle">
+          <h2>Should you...</h2>
+          {data.choiceOne && (
+            <div>
+            <Link style={{color: "red"}} to={data.choiceOne.fields.slug}>{data.choiceOne.frontmatter.title}</Link>
+            </div>
+          )}
+          {data.choiceTwo && (
+            <div>
+              <Link style={{color: "red"}} to={data.choiceTwo.fields.slug}>{data.choiceTwo.frontmatter.title}</Link>
+            </div>
+          )}
+          {!data.choiceOne && !data.choiceTwo && (
+            <div>
+              <Link style={{color: "red"}} to="/zombies/lose">see your fate?</Link>
+            </div>
+          )}
         </div>
-      )}
-      {!data.choiceOne && !data.choiceTwo && (
-        <div>
-          <Link style={{color: "#000000"}} to="/zombies/lose">BRAINZZzz</Link>
-        </div>
-      )}
+        
+      </div>
     </div>
+    
   )
 }
